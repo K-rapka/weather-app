ba72afd49420a604be4a41cc7f2a4dddd6fe57f2
alexandr <sasha.drupp24@gmail.com>
2023-05-18 19:11:34 +0200

    Styles

--- src/fullscreen/fullscreen.jsx
+++ src/fullscreen/fullscreen.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
 export const FullscreenApp = () => {
+
+    const weather = 'rainy'
+
     return (
-        <div className="main sunny">
+        <div className={`main ${weather}`}>
             <div className="container">
-                <div className="weatherScreen">
+                <div className={`weatherScreen ${weather}`}>
                     <form><input className="search" type="text" placeholder="Search" /></form>
-                    <div>
-                        <h1>Country</h1>
+                    <div className={`weather ${weather}`}>
+                        <div>Azerbaijan, Baku</div>
                         <div>13°C</div>
                         <div>Sunny</div>
                     </div>
-                    <div>
+                    <div className="wind">
                         <div>Humidity: 40%</div>
                         <div>Wind Speed: 13 km/h</div>
                     </div>
                 </div>
-                <div className="pattern"></div>
+                <div className={`pattern ${weather}`}></div>
             </div>
 
         </div>
